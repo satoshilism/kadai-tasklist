7a636b1936cff874c62eefaba1ce2204d693a6dc
satoshi.matsubayashi <satoshilism@gmail.com>
2018-09-09 08:23:32 +0000

    CRUD task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,15 @@ class TasksController < ApplicationController
   end 
   
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
   end 
   
   def edit
@@ -23,6 +32,13 @@ class TasksController < ApplicationController
   
   def destroy
   end 
+  
+   private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
   
 
 end
